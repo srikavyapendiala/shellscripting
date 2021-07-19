e24982f2ba7ae5862f2a9c1592ac6a40f9921ba3
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-19 12:18:46 +0530

    Update users.sh

--- todo/components/users.sh
+++ todo/components/users.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 source components/common.sh
-HEAD "Set Hostname and Update repo"
-REPEAT
-Stat $?
+OS_PREREQ
+STAT $?
+Head "check java version"
+java -version
+Head "install java 8 version"
+apt-get install openjdk-8-jdk -y &>>${LOG}
+STAT $?
+Head "Install maven"
+apt install maven -y &>>$LOG
+STAT $?
 
-HEAD "Install java-openjdk"
-apt-get install openjdk-8-jdk-headless -y &>>${LOG}
-Stat $?
+DOWNLOAD_COMPONENT
+STAT $?
 
-HEAD "Installing Maven"
-apt install maven -y &>>${LOG}
-Stat $?
+Head "Create package"
+mvn clean package &>>$LOG
+STAT $?
 
-HEAD "Cloning the repo"
-GIT_CLONE
-Stat $?
+Head "Create Users Service"
+mv /root/todoshell/users/systemd.service /etc/systemd/system/users.service
+STAT $?
 
-HEAD "cleaning the maven package"
-mvn clean package &>>${LOG}
-Stat $?
-
-HEAD "Now move the user services"
-mv /root/shellscripting-todo/todo/users/systemd.service /etc/systemd/system/users.service
-Stat $?
-
-
-HEAD "Restart the services"
-systemctl daemon-reload
-systemctl start users.service
-systemctl status users.service
-Stat $?
+Head "Start users service"
+systemctl daemon-reload && systemctl start users && systemctl status users
+STAT $?
