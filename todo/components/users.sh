483bcd96d43c62b5a3ce871dae5a649d614a97d7
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-19 12:26:01 +0530

    Update users.sh

--- todo/components/users.sh
+++ todo/components/users.sh
@@ -19,7 +19,7 @@ mvn clean package &>>$LOG
 STAT $?
 
 Head "Create Users Service"
-mv /root/todoshell/users/systemd.service /etc/systemd/system/users.service
+mv /root/shellscripting/todo/users/systemd.service /etc/systemd/system/users.service
 STAT $?
 
 Head "Start users service"
