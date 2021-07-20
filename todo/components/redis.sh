99c8f24f726c24db35f161d0695ae860d5e70360
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-20 19:22:45 +0530

    Update redis.sh

--- todo/components/redis.sh
+++ todo/components/redis.sh
@@ -5,9 +5,12 @@ OS_PREREQ
 
 Head "Install redis server"
 sudo apt install redis-server -y >>"${LOG}"
+STAT $?
 
 Head "Change ip address in redis config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+STAT $?
 
 Head "Restart redis server"
-systemctl restart redis
+systemctl start redis
+STAT $?
