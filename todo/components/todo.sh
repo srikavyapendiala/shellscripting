5069026f560e7df0f33086069079560fbd6de51e
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-28 17:19:10 +0530

    Update todo.sh

--- todo/components/todo.sh
+++ todo/components/todo.sh
@@ -5,17 +5,19 @@ OS_PREREQ
 
 Head "Installing npm"
 apt install npm -y &>>$LOG
-STAT $?
 
 DOWNLOAD_COMPONENT
 
+Head "Install npm"
+npm install &>>$LOG
+STAT $?
 
 Head "Update Redis IP in service File"
-sed -i -e "s/DNSREDIS/redis.kavya.website/" /root/todoshell/todo/todo/systemd.service
-
+sed -i -e "s/DNSREDIS/redis.zsdevtraining.online/" /root/todoshell/todo/systemd.service
+Stat $?
 
 Head  "Create service file"
-mv /root/todoshell/todo/todo/systemd.service /etc/systemd/system/todo.service
+mv /root/todoshell/todo/systemd.service /etc/systemd/system/todo.service
 
 Head "Start Todo Service"
 systemctl daemon-reload && systemctl start todo && systemctl status todo
