8364dea2bdbf2cf8ca4d7fb78c5b00b6f0cc7a01
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-28 17:29:38 +0530

    Update todo.sh

--- todo/components/todo.sh
+++ todo/components/todo.sh
@@ -13,7 +13,7 @@ npm install &>>$LOG
 STAT $?
 
 Head "Update Redis IP in service File"
-sed -i -e "s/DNSREDIS/redis.kavya.website/" /root/todoshell/todo/todo/systemd.service
+sed -i "s/DNSREDIS/redis.kavya.website/" /root/todoshell/todo/todo/systemd.service
 STAT $?
 
 Head  "Create service file"
